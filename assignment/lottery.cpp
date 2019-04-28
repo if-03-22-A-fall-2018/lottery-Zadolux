026502fa4d706aef0b71acb5102e39f7d71b8db2
Zadolux <zadolux@inscomers.net>
2019-04-28 21:05:59 +0200

    Tests show

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -13,25 +13,26 @@
 #include "lottery.h"
 #include "general.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #define UUID_LEN 40
 #define MAX_TIP_LEN 17
 #define MAX_LINE_LEN (UUID_LEN + 1 + MAX_TIP_LEN + 1)
 
-struct fileinfo
+typedef struct FileInfo* FInfo;
+struct FileInfo
 {
   FILE* fd;
   char csv_separator;
 };
 
-struct fileinfo* finfo = malloc(sizeof(struct fileinfo));
-
+FInfo file_info = (FInfo) malloc(sizeof(struct FileInfo));
 
 bool init_lottery(const char *csv_file, char csv_separator)
 {
-  finfo->fd = fopen(csv_file, "r");
-  finfo->csv_separator = csv_separator;
-  return finfo->fd != 0;
+  file_info->fd = fopen(csv_file, "r");
+  file_info->csv_separator = csv_separator;
+  return file_info->fd != 0;
 }
 
 bool get_tip(int tip_number, int tip[TIP_SIZE])
